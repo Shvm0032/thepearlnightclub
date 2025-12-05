4ae62b248dfceb1ba8ca3e783a0b48ec6141bf21
developmentrankmantra-sudo <developmentrankmantra@gmail.com>
2025-12-05 10:03:34 +0530

    verel error

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
-import Footer from './../components/Footer';
+import Footer from "../components/Footer";
 
 const geistSans = Geist({ subsets: ["latin"] });
 const geistMono = Geist_Mono({ subsets: ["latin"] });
@@ -25,7 +25,7 @@ export default function RootLayout({ children }) {
       <body className={`${geistSans.className} bg-background text-foreground`}>
         <Header />
         <main>{children}</main>
-        <Footer />
+        <Footer/>
       </body>
     </html>
   );
